58c87f59589406117ef05b5986eb63216bf19620
Devlin <webmasterdevlin@gmail.com>
2020-02-14 16:24:27 +0100

    updated versions.tf

--- azure/versions.tf
+++ azure/versions.tf
@@ -1,3 +1,7 @@
+# terraform 0.12.0 has fixed the issues with
+# expressions, type system, error handling,
+# iteration, conditionals, terraform state,
+# testing, workflow, and modules
 terraform {
   required_version = ">= 0.12.0"
 }
