cdcf0228bc33bb2402e782daccf272febe5832c4
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-02-14 01:32:49 +0100

    rollback

--- azure/main.tf
+++ azure/main.tf
@@ -3,3 +3,55 @@ resource "azurerm_resource_group" "test" {
   location = var.location
 }
 
+resource "azurerm_app_service_plan" "test" {
+  name                = "my-app-service-plan-1"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "test" {
+  name                = var.app-service-name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  app_service_plan_id = azurerm_app_service_plan.test.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "MY_SECRET_KEY" = var.app-settings-value-x
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+  }
+}
+
+resource "azurerm_sql_server" "test" {
+  name                         = "foo-sql-server-1"
+  resource_group_name          = azurerm_resource_group.test.name
+  location                     = azurerm_resource_group.test.location
+  version                      = "12.0"
+  administrator_login          = var.admin-login-devlin
+  administrator_login_password = var.admin-pass-devlin
+}
+
+resource "azurerm_sql_database" "test" {
+  name                = "foo-sql-database-1"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  server_name         = azurerm_sql_server.test.name
+
+  tags = {
+    environment = "production"
+  }
+}
