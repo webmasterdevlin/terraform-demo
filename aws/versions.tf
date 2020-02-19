9197f078c57b3f78936b62c77954367e97d11d9d
Devlin <webmasterdevlin@gmail.com>
2020-02-19 10:47:03 +0100

    edited the version docs

--- aws/versions.tf
+++ aws/versions.tf
@@ -1,6 +1,10 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
 # This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# Terraform 0.12.0 has fixed the issues with
+# expressions, type system, error handling,
+# iteration, conditionals, terraform state,
+# testing, workflow, and modules
 # ----------------------------------------------------------------------------------------------------------------------
 
 terraform {
--- azure/versions.tf
+++ azure/versions.tf
@@ -1,7 +1,12 @@
-# terraform 0.12.0 has fixed the issues with
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# Terraform 0.12.0 has fixed the issues with
 # expressions, type system, error handling,
 # iteration, conditionals, terraform state,
 # testing, workflow, and modules
+# ----------------------------------------------------------------------------------------------------------------------
+
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.12"
 }
